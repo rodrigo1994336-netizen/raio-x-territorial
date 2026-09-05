4d6062a4723a007b0415fe74367b6199c366b56f
rodrigo1994336-netizen <rodrigo1994336@gmail.com>
2026-09-05 10:28:14 -0300

    feat: activate V18 street-access report runtime

---
+++ report_v18_patch.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import report_api as base
+import report_v13_patch as v13patch
+from live_report_adapter_v18 import generate_live_report
+
+base.generate_live_report=generate_live_report
+v13patch.generate_live_report=generate_live_report
+base.APP_VERSION='0.31.0-progressive-v18-street-access'
+
+print('RX_REPORT_V18_RUNTIME=street_access_connected',flush=True)
